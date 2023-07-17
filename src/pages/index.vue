e5a66f844e82539a7f4917824342b34eea8cee95
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-07-17 14:29:33 -0300

    init

---
+++ src/pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+	<header class="p-5 ">
+		<nav class="flex flex-row items-center justify-between container mx-auto max-w-2xl">
+      <NuxtLink to="/">Ashou</NuxtLink>
+			<ul class="flex flex-row items-center space-x-2">
+				<li></li>
+				<li></li>
+			</ul>
+		</nav>
+	</header>
+</template>
