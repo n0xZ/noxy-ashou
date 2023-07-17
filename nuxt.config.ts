de7022152f10865ece8df733a622755e4633fdbb
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-07-17 15:44:18 -0300

    chore: add new modules related to fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	devtools: { enabled: true },
-	modules: ['@nuxtjs/tailwindcss'],
+	modules: ['@nuxtjs/tailwindcss', '@nuxtjs/google-fonts'],
 	srcDir: 'src/',
+	googleFonts: {
+		download: true,
+		families: { Inter: true },
+		useStylesheet: true,
+	},
 })
