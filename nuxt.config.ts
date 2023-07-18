4fb969897d3c12597e79c6a291b16a0c15c14720
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-07-18 15:22:05 -0300

    chore: add nuxt-auth module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	devtools: { enabled: true },
-	modules: ['@nuxtjs/tailwindcss', '@nuxtjs/google-fonts'],
+	modules: [
+		'@nuxtjs/tailwindcss',
+		'@nuxtjs/google-fonts',
+		'@sidebase/nuxt-auth',
+	],
 	srcDir: 'src/',
 	googleFonts: {
 		download: true,
