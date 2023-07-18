3ab0e4ec28963dee3e3b5dc531b2cd1701c88d19
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-07-18 10:55:41 -0300

    set dark mode as class

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,7 @@ import type { Config } from 'tailwindcss'
 import defaultTheme from 'tailwindcss/defaultTheme'
 
 export default <Partial<Config>>{
+	darkMode: 'class',
 	theme: {
 		...defaultTheme,
 		extend: {
