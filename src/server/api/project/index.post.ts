0da5c4b92ac11945049ed093435e599c4df05639
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-07-19 10:54:54 -0300

    chore: setup POST project route

---
+++ src/server/api/project/index.post.ts
@@ -0,0 +1,27 @@
+import { getServerSession } from '#auth'
+import { prisma } from '~/db/prisma'
+import { createProjectSchema } from '~/utils/zod'
+
+// It creates an project with its name, description and siteUrl, and its related user.
+export default defineEventHandler(async (ev) => {
+	try {
+		const sess = await getServerSession(ev)
+
+		if (!sess)
+			throw createError({ statusCode: 400, statusMessage: 'No session found' })
+		const body = await readBody(ev)
+		const { description, name, siteUrl } = createProjectSchema.parse(body)
+		const project = await prisma.project.create({
+			data: {
+				name,
+				description,
+				siteUrl,
+				user: { connect: { email: sess.user?.email ?? '' } },
+			},
+		})
+		return { project }
+	} catch (e) {
+		if (e instanceof Error)
+			throw createError({ message: e.message, statusCode: 500 })
+	}
+})
