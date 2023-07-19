a1aa71538d7d66f16667c5099eafcd5b56bc90e2
n0xZ <o.gonzalo@hotmail.com>
2023-07-19 17:10:23 -0300

    chore: add avatar comp

---
+++ src/components/ui/avatar.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+	defineProps<{ src?: string; alt: string }>()
+</script>
+
+<template>
+	<img
+		:src="src"
+		:alt="alt"
+		width="56"
+		height="56"
+		class="rounded-full hover:opacity-60 duration-100 ease-in aspect-auto"
+	/>
+</template>
