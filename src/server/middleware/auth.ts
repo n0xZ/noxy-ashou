cac7a11203a20d4eeb83c673f7793a943e14588d
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-07-24 13:11:09 -0300

    fix issue related to middleware catching FE routes

--- src/server/middleware/auth.ts
+++ src/server/middleware/auth.ts
@@ -1,7 +1,11 @@
 import { getServerSession } from '#auth'
 export default defineEventHandler(async (event) => {
 	const session = await getServerSession(event)
-	if (!session) {
+	if (!event.node.req.url?.startsWith('/api/protected/middleware')) {
+		return
+	}
+
+	if (!session && event.path.includes('/api')) {
 		throw createError({
 			statusMessage: 'You are not authorized to perform this action.',
 			statusCode: 403,
