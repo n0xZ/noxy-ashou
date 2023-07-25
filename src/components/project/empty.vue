916aa11a8e9047a14a212db4f46c2ee311f54a13
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-07-25 17:06:05 -0300

    chore: setup empty project to display if user does not have existing projects

---
+++ src/components/project/empty.vue
@@ -0,0 +1,8 @@
+<template>
+	<section class="flex flex-col items-center justify-center h-screen space-y-1 text-center">
+		<UiParagraph>It seems you have no project defined yet.</UiParagraph>
+		<NuxtLink to="/home/project/create" class="underline"
+			>Click here to create your project</NuxtLink
+		>
+	</section>
+</template>
