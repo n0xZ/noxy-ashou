4134aea52c06fa6c644425eb344c194e894c3607
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-07-25 15:38:03 -0300

    chore: setup project skeleton

---
+++ src/components/project/skeleton.vue
@@ -0,0 +1,76 @@
+<script setup lang="ts"></script>
+
+<template>
+	<section class="grid w-full grid-rows-3 mx-auto mt-10 gap-9">
+		<article class="flex flex-col w-full h-32 gap-1 p-3 rounded-md shadow-md">
+			<div class="w-32 h-4 bg-gray-200 rounded-md animate-pulse"></div>
+			<div class="w-3/4 h-4 bg-gray-200 rounded-md animate-pulse"></div>
+			<button
+				type="button"
+				class="self-end w-full p-2 text-center text-white rounded-md xl:w-1/6 place-self-end bg-secondary-foreground"
+			>
+				Cargando...
+			</button>
+		</article>
+		<article class="flex flex-col w-full h-32 gap-1 p-3 rounded-md shadow-md">
+			<div class="w-32 h-4 bg-gray-200 rounded-md animate-pulse"></div>
+			<div class="w-3/4 h-4 bg-gray-200 rounded-md animate-pulse"></div>
+			<button
+				type="button"
+				class="self-end w-full p-2 text-center text-white rounded-md xl:w-1/6 place-self-end bg-secondary-foreground"
+			>
+				Cargando...
+			</button>
+		</article>
+		<article class="flex flex-col w-full h-32 gap-1 p-3 rounded-md shadow-md">
+			<div class="w-32 h-4 bg-gray-200 rounded-md animate-pulse"></div>
+			<div class="w-3/4 h-4 bg-gray-200 rounded-md animate-pulse"></div>
+			<button
+				type="button"
+				class="self-end w-full p-2 text-center text-white rounded-md xl:w-1/6 place-self-end bg-secondary-foreground"
+			>
+				Cargando...
+			</button>
+		</article>
+		<article class="flex flex-col w-full h-32 gap-1 p-3 rounded-md shadow-md">
+			<div class="w-32 h-4 bg-gray-200 rounded-md animate-pulse"></div>
+			<div class="w-3/4 h-4 bg-gray-200 rounded-md animate-pulse"></div>
+			<button
+				type="button"
+				class="self-end w-full p-2 text-center text-white rounded-md xl:w-1/6 place-self-end bg-secondary-foreground"
+			>
+				Cargando...
+			</button>
+		</article>
+		<article class="flex flex-col w-full h-32 gap-1 p-3 rounded-md shadow-md">
+			<div class="w-32 h-4 bg-gray-200 rounded-md animate-pulse"></div>
+			<div class="w-3/4 h-4 bg-gray-200 rounded-md animate-pulse"></div>
+			<button
+				type="button"
+				class="self-end w-full p-2 text-center text-white rounded-md xl:w-1/6 place-self-end bg-secondary-foreground"
+			>
+				Cargando...
+			</button>
+		</article>
+		<article class="flex flex-col w-full h-32 gap-1 p-3 rounded-md shadow-md">
+			<div class="w-32 h-4 bg-gray-200 rounded-md animate-pulse"></div>
+			<div class="w-3/4 h-4 bg-gray-200 rounded-md animate-pulse"></div>
+			<button
+				type="button"
+				class="self-end w-full p-2 text-center text-white rounded-md xl:w-1/6 place-self-end bg-secondary-foreground"
+			>
+				Cargando...
+			</button>
+		</article>
+		<article class="flex flex-col w-full h-32 gap-1 p-3 rounded-md shadow-md">
+			<div class="w-32 h-4 bg-gray-200 rounded-md animate-pulse"></div>
+			<div class="w-3/4 h-4 bg-gray-200 rounded-md animate-pulse"></div>
+			<button
+				type="button"
+				class="self-end w-full p-2 text-center text-white rounded-md xl:w-1/6 place-self-end bg-secondary-foreground"
+			>
+				Cargando...
+			</button>
+		</article>
+	</section>
+</template>
