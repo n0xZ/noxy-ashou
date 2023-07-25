9550e5b89e8e91e22af89c86068b8d42e717c91c
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-07-25 15:47:57 -0300

    chore: Setup project list

---
+++ src/components/project/list.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+	import { Project } from '@prisma/client'
+	import ProjectItem from './item.vue'
+	type Props = {
+		projects: Project[]
+	}
+	const props = defineProps<Props>()
+</script>
+
+<template>
+	<section class="grid w-full grid-rows-3 mx-auto mt-10 gap-9">
+		<ProjectItem
+			v-for="project in props.projects"
+			:project="project"
+			:key="project.id"
+		/>
+	</section>
+</template>
