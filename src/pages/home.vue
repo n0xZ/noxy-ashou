51e2ecb1db83d0c911a0d76411a5344f525e2d95
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-07-25 00:04:18 -0300

    add home menubar

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,44 +1,18 @@
 <script setup lang="ts">
-	import {
-		Menu,
-		MenuItem,
-		MenuPositioner,
-		MenuTrigger,
-		MenuContent,
-	} from '@ark-ui/vue'
-	const { signOut } = useAuth()
-	const { data } = useAuth()
+	definePageMeta({ auth: true })
 </script>
 
 <template>
-	<header class="p-5">
-		<nav
-			class="flex flex-row items-center justify-between container mx-auto max-w-2xl"
-		>
-			<NuxtLink to="/">Ashou</NuxtLink>
-			<ul class="flex flex-row items-center space-x-2">
-				<Menu>
-					<MenuTrigger class="hover:cursor-pointer">
-						<UiAvatar :src="data?.user?.email" :alt="'Imagen de Noxy'" />
-					</MenuTrigger>
-					<Teleport to="body">
-						<MenuPositioner>
-							<MenuContent
-								class="w-48 h-64 rounded-md shadow-lg overflow-hidden outline-none animate-in fade-in p-3 duration-300 font-inter"
-								><MenuItem id="signOut" class="w-full h-8"
-									><button
-										class="font-medium w-full h-full hover:opacity-70 rounded-md"
-										@click="signOut({ callbackUrl: '/' })"
-									>
-										Cerrar sesión
-									</button></MenuItem
-								></MenuContent
-							>
-						</MenuPositioner>
-					</Teleport>
-				</Menu>
-			</ul>
-		</nav>
-	</header>
-	<NuxtPage />
+	<div>
+		<header class="p-5">
+			<nav
+				class="container flex flex-row items-center justify-between max-w-4xl mx-auto"
+			>
+				<NuxtLink to="/home">Home</NuxtLink>
+				<HomeMenubar />
+			</nav>
+		</header>
+
+		<NuxtPage />
+	</div>
 </template>
