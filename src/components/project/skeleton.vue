05f88cbe4d7787ff87cdef3ba9bab95d3098c9d0
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-07-25 15:47:15 -0300

    chore: update skeleton properties

--- src/components/project/skeleton.vue
+++ src/components/project/skeleton.vue
@@ -1,73 +1,91 @@
-<script setup lang="ts"></script>
-
 <template>
 	<section class="grid w-full grid-rows-3 mx-auto mt-10 gap-9">
-		<article class="flex flex-col w-full h-32 gap-1 p-3 rounded-md shadow-md">
-			<div class="w-32 h-4 bg-gray-200 rounded-md animate-pulse"></div>
-			<div class="w-3/4 h-4 bg-gray-200 rounded-md animate-pulse"></div>
+		<article
+			class="flex flex-col justify-between w-full h-32 gap-1 p-3 rounded-md shadow-md"
+		>
+			<div class="flex flex-col space-y-2">
+				<div class="w-32 h-4 bg-gray-200 rounded-md animate-pulse"></div>
+				<div class="w-3/4 h-4 bg-gray-200 rounded-md animate-pulse"></div>
+			</div>
 			<button
 				type="button"
 				class="self-end w-full p-2 text-center text-white rounded-md xl:w-1/6 place-self-end bg-secondary-foreground"
+				disabled
 			>
 				Cargando...
 			</button>
 		</article>
-		<article class="flex flex-col w-full h-32 gap-1 p-3 rounded-md shadow-md">
-			<div class="w-32 h-4 bg-gray-200 rounded-md animate-pulse"></div>
-			<div class="w-3/4 h-4 bg-gray-200 rounded-md animate-pulse"></div>
+		<article
+			class="flex flex-col justify-between w-full h-32 gap-1 p-3 rounded-md shadow-md"
+		>
+			<div class="flex flex-col space-y-2">
+				<div class="w-32 h-4 bg-gray-200 rounded-md animate-pulse"></div>
+				<div class="w-3/4 h-4 bg-gray-200 rounded-md animate-pulse"></div>
+			</div>
 			<button
 				type="button"
 				class="self-end w-full p-2 text-center text-white rounded-md xl:w-1/6 place-self-end bg-secondary-foreground"
+				disabled
 			>
 				Cargando...
 			</button>
 		</article>
-		<article class="flex flex-col w-full h-32 gap-1 p-3 rounded-md shadow-md">
-			<div class="w-32 h-4 bg-gray-200 rounded-md animate-pulse"></div>
-			<div class="w-3/4 h-4 bg-gray-200 rounded-md animate-pulse"></div>
+		<article
+			class="flex flex-col justify-between w-full h-32 gap-1 p-3 rounded-md shadow-md"
+		>
+			<div class="flex flex-col space-y-2">
+				<div class="w-32 h-4 bg-gray-200 rounded-md animate-pulse"></div>
+				<div class="w-3/4 h-4 bg-gray-200 rounded-md animate-pulse"></div>
+			</div>
 			<button
 				type="button"
 				class="self-end w-full p-2 text-center text-white rounded-md xl:w-1/6 place-self-end bg-secondary-foreground"
+				disabled
 			>
 				Cargando...
 			</button>
 		</article>
-		<article class="flex flex-col w-full h-32 gap-1 p-3 rounded-md shadow-md">
-			<div class="w-32 h-4 bg-gray-200 rounded-md animate-pulse"></div>
-			<div class="w-3/4 h-4 bg-gray-200 rounded-md animate-pulse"></div>
+		<article
+			class="flex flex-col justify-between w-full h-32 gap-1 p-3 rounded-md shadow-md"
+		>
+			<div class="flex flex-col space-y-2">
+				<div class="w-32 h-4 bg-gray-200 rounded-md animate-pulse"></div>
+				<div class="w-3/4 h-4 bg-gray-200 rounded-md animate-pulse"></div>
+			</div>
 			<button
 				type="button"
 				class="self-end w-full p-2 text-center text-white rounded-md xl:w-1/6 place-self-end bg-secondary-foreground"
+				disabled
 			>
 				Cargando...
 			</button>
 		</article>
-		<article class="flex flex-col w-full h-32 gap-1 p-3 rounded-md shadow-md">
-			<div class="w-32 h-4 bg-gray-200 rounded-md animate-pulse"></div>
-			<div class="w-3/4 h-4 bg-gray-200 rounded-md animate-pulse"></div>
+		<article
+			class="flex flex-col justify-between w-full h-32 gap-1 p-3 rounded-md shadow-md"
+		>
+			<div class="flex flex-col space-y-2">
+				<div class="w-32 h-4 bg-gray-200 rounded-md animate-pulse"></div>
+				<div class="w-3/4 h-4 bg-gray-200 rounded-md animate-pulse"></div>
+			</div>
 			<button
 				type="button"
 				class="self-end w-full p-2 text-center text-white rounded-md xl:w-1/6 place-self-end bg-secondary-foreground"
+				disabled
 			>
 				Cargando...
 			</button>
 		</article>
-		<article class="flex flex-col w-full h-32 gap-1 p-3 rounded-md shadow-md">
-			<div class="w-32 h-4 bg-gray-200 rounded-md animate-pulse"></div>
-			<div class="w-3/4 h-4 bg-gray-200 rounded-md animate-pulse"></div>
+		<article
+			class="flex flex-col justify-between w-full h-32 gap-1 p-3 rounded-md shadow-md"
+		>
+			<div class="flex flex-col space-y-2">
+				<div class="w-32 h-4 bg-gray-200 rounded-md animate-pulse"></div>
+				<div class="w-3/4 h-4 bg-gray-200 rounded-md animate-pulse"></div>
+			</div>
 			<button
 				type="button"
 				class="self-end w-full p-2 text-center text-white rounded-md xl:w-1/6 place-self-end bg-secondary-foreground"
-			>
-				Cargando...
-			</button>
-		</article>
-		<article class="flex flex-col w-full h-32 gap-1 p-3 rounded-md shadow-md">
-			<div class="w-32 h-4 bg-gray-200 rounded-md animate-pulse"></div>
-			<div class="w-3/4 h-4 bg-gray-200 rounded-md animate-pulse"></div>
-			<button
-				type="button"
-				class="self-end w-full p-2 text-center text-white rounded-md xl:w-1/6 place-self-end bg-secondary-foreground"
+				disabled
 			>
 				Cargando...
 			</button>
