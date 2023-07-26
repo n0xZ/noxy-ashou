c5082c832cc3f0a78d63ac38b3113c2b1bca05c7
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-07-26 09:56:18 -0300

    chore: add create-project anchor

--- src/components/home/menubar.vue
+++ src/components/home/menubar.vue
@@ -14,31 +14,45 @@
 </script>
 
 <template>
-	<MenubarRoot class="font-inter">
+	<MenubarRoot>
 		<MenubarMenu value="avatar">
 			<MenubarTrigger
-				class="gap-1 font-semibold outline-none select-none data-[highlighted]:drop-shadow-md duration-100 data-[state=open]:bg-green4"
+				class="gap-1 font-semibold outline-none select-none data-[highlighted]:drop-shadow-md duration-100"
 			>
-				<UiAvatar :src="data?.user?.image ?? ''" :alt="'Imagen de noxy'" />
+				<UiAvatar
+					:src="data?.user?.image ?? ''"
+					:alt="'Avatar de ' + data?.user?.name"
+				/>
 			</MenubarTrigger>
 			<MenubarPortal>
 				<MenubarContent
-					class="min-w-[220px] bg-background rounded-md p-[5px] gap-3 animate-in duration-100 shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)]"
+					class="min-w-[220px] bg-background rounded-md font-inter p-[5px] gap-3 animate-in duration-100"
 					align="start"
 					:side-offset="5"
 					:align-offset="-3"
 				>
 					<MenubarItem
-						class="group text-[13px] leading-none font-semibold rounded flex items-center h-[25px] px-[10px] relative select-none outline-none data-[state=open]:bg-green4 data-[state=open]:text-grass11 data-[highlighted]:bg-gradient-to-br data-[highlighted]:from-sky-100 data-[highlighted]:to-sky-300 duration-200 data-[highlighted]:text-green1 data-[highlighted]:data-[state=open]:text-green1 data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none"
+						class="group text-[13px] leading-none font-semibold rounded flex space-x-1 items-center h-[25px] px-[10px] relative select-none outline-none data-[state=open]:bg-secondary/10 data-[highlighted]:bg-secondary data-[disabled]:pointer-events-none"
 					>
-						{{ data?.user?.name }}
+						<Icon name="material-symbols:account-circle" class="w-4 h-4" />
+						<span> {{ data?.user?.name }}</span>
 					</MenubarItem>
 					<MenubarSeparator class="h-[1px] bg-gray-100 m-[3px]" />
 					<MenubarSub>
 						<MenubarSubTrigger
-							class="group text-[13px] leading-none text-grass11 rounded flex items-center h-[25px] px-[10px] relative select-none outline-none data-[state=open]:bg-green4 data-[state=open]:text-grass11 data-[highlighted]:bg-gradient-to-br data-[highlighted]:from-green9 data-[highlighted]:to-green10 data-[highlighted]:text-green1 data-[highlighted]:data-[state=open]:text-green1 data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none"
+							class="group text-[13px] leading-none rounded flex hover:cursor-pointer items-center h-[25px] px-[10px] relative select-none outline-none data-[highlighted]:bg-secondary data-[disabled]:pointer-events-none"
 						>
-							<button @click="signOut({ callbackUrl: '/' })">Cerrar sesión</button>
+							<NuxtLink to="/home/project/create" class="flex flex-row items-center space-x-1">
+								<Icon name="material-symbols:assignment-add" class="w-4 h-4" />
+								<span>New proyect</span>
+							</NuxtLink>
+						</MenubarSubTrigger>
+						<MenubarSubTrigger
+							class="group text-[13px] leading-none rounded flex flex-row space-x-1 hover:cursor-pointer items-center h-[25px] px-[10px] relative select-none outline-none data-[highlighted]:bg-secondary data-[disabled]:pointer-events-none"
+							@click="signOut({ callbackUrl: '/' })"
+						>
+							<Icon name="material-symbols:logout" class="w-4 h-4" />
+							<span>Sign out</span>
 						</MenubarSubTrigger>
 					</MenubarSub>
 				</MenubarContent>
