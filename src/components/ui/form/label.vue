5d9d9acf4cda0591ba1b6db32b5ca25b4baf2e1e
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-07-31 09:38:00 -0300

    chore: add shadcn label comp

---
+++ src/components/ui/form/label.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+	import { Label } from 'radix-vue'
+	defineProps<{ for?: string }>()
+</script>
+<template>
+	<Label
+		class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+		:for="$props.for"
+	>
+		<slot />
+	</Label>
+</template>
