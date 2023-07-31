4348a3fd26d0e5c2267a265852acf7a1d9aeaf2b
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-07-31 14:54:40 -0300

    override class props

--- src/components/ui/button.vue
+++ src/components/ui/button.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 	defineProps<{
+		class?: string
+		title?: string
 		onClick?: () => void
 		type?: 'button' | 'submit'
 		disabled?: boolean
@@ -8,7 +10,7 @@
 <template>
 	<button
 		@click="onClick"
-		class="inline-flex items-center justify-center h-10 px-4 py-2 text-sm font-medium transition-colors rounded-md ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90"
+		:class="`inline-flex items-center justify-center h-10 px-4 py-2 text-sm font-medium transition-colors rounded-md ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 ${$props.class}`"
 		:disabled="disabled"
 	>
 		<slot />
