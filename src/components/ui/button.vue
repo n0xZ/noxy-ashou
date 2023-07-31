7105ef8e06828027fb30a1e17611f222564e4ed6
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-07-31 09:37:50 -0300

    chore: add shadcn ui button

---
+++ src/components/ui/button.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+	defineProps<{
+		onClick?: () => void
+		type?: 'button' | 'submit'
+		disabled?: boolean
+	}>()
+</script>
+<template>
+	<button
+		@click="onClick"
+		class="inline-flex items-center justify-center h-10 px-4 py-2 text-sm font-medium transition-colors rounded-md ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90"
+		:disabled="disabled"
+	>
+		<slot />
+	</button>
+</template>
