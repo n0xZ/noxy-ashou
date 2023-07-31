85b30a39a9ced5c95e093f0e43203f35752283bb
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-07-31 11:53:15 -0300

    chore: setup project models

---
+++ src/models/project.server.ts
@@ -0,0 +1,59 @@
+import { prisma } from '../db/prisma'
+import { ProjectOutput } from '../utils/valibot'
+
+export async function projectsByUserCredential({ email }: { email?: string }) {
+	try {
+		const projects = await prisma.project.findMany({ where: { user: { email } } })
+		return { projects }
+	} catch (e) {
+		if (e instanceof Error) console.error(e)
+	}
+}
+
+export async function createProject({
+	description,
+	name,
+	siteUrl,
+	email,
+}: ProjectOutput & { email?: string }) {
+	try {
+		const project = await prisma.project.create({
+			data: {
+				name,
+				description,
+				siteUrl,
+				user: { connect: { email } },
+			},
+		})
+		return { project }
+	} catch (e) {
+		if (e instanceof Error) console.error(e)
+	}
+}
+export async function updateProject({
+	id,
+	description,
+	name,
+	siteUrl,
+}: Partial<ProjectOutput> & { id: string }) {
+	try {
+		const project = await prisma.project.update({
+			data: { description, name, siteUrl },
+			where: { id },
+		})
+		return { project }
+	} catch (e) {
+		if (e instanceof Error) console.error(e)
+	}
+}
+
+export async function deleteProject({ id }: { id: string }) {
+	try {
+		const project = await prisma.project.delete({
+			where: { id },
+		})
+		return { project }
+	} catch (e) {
+		if (e instanceof Error) console.error(e)
+	}
+}
