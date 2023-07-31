9c548a6e3371c75eb85f2b7726c8a10b3fa22b90
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-07-31 13:45:19 -0300

    chore: add prisma adapter

--- src/server/api/auth/[...].ts
+++ src/server/api/auth/[...].ts
@@ -1,6 +1,10 @@
 import { NuxtAuthHandler } from '#auth'
 import GithubProvider from 'next-auth/providers/github'
+import { PrismaAdapter } from '@auth/prisma-adapter'
+import { prisma } from '~/server/db/prisma'
 export default NuxtAuthHandler({
+	///@ts-ignore
+	adapter: PrismaAdapter(prisma),
 	providers: [
 		// @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
 		GithubProvider.default({
