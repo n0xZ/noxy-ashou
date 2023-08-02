371a31ee933662352f121f67c4dbc7e87bb72dc7
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-08-02 16:20:59 -0300

    fix: target node request url on conditional statement to check if user's already logged in and if it is consuming private endpoints

--- src/server/middleware/auth.ts
+++ src/server/middleware/auth.ts
@@ -1,11 +1,11 @@
 import { getServerSession } from '#auth'
 export default defineEventHandler(async (event) => {
 	const session = await getServerSession(event)
-	if (!event.node.req.url?.startsWith('/api/protected/middleware')) {
+	if (!event.node.req.url?.startsWith('/api/')) {
 		return
 	}
 
-	if (!session && event.path.includes('/api')) {
+	if (!session && event.node.req.url?.startsWith('/api')) {
 		throw createError({
 			statusMessage: 'You are not authorized to perform this action.',
 			statusCode: 403,
