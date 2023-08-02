04cdf2115d5bf50929576da305d2de6bfcb50452
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-08-02 18:13:19 -0300

    chore: add `contains errors` and `error from field` function

--- src/utils/valibot.ts
+++ src/utils/valibot.ts
@@ -1,4 +1,4 @@
-import { object, string, minLength, url, Output } from 'valibot'
+import { object, string, minLength, url, Output, Issues } from 'valibot'
 
 export const createProjectSchema = object({
 	name: string([minLength(3, 'Name is required')]),
@@ -6,3 +6,14 @@ export const createProjectSchema = object({
 	siteUrl: string([url('Invalid site URL')]),
 })
 export type ProjectOutput = Output<typeof createProjectSchema>
+
+// It returns an boolean if the key exists in the issues
+
+export const containsErrors = (key: keyof ProjectOutput, issues: Issues) =>
+	issues && issues.some((i) => i.path?.[0].key === key)
+
+//It returns an error message related to the key and issues specified
+export const errorFromField = (key: keyof ProjectOutput, issues: Issues) => {
+	const error = issues?.find((i) => i.path?.[0].key === key)
+	return error?.message
+}
