2e4d8644adece84c44af5f14053104d4f038b68e
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-08-02 12:42:27 -0300

    Chore: add DELETE registry api route

---
+++ src/server/api/project/registry/index.delete.ts
@@ -0,0 +1,21 @@
+import { prisma } from '@/server/db/prisma'
+
+// It deletes an project based on specified ID.
+export default defineEventHandler(async (ev) => {
+	try {
+		const body = await readBody(ev)
+		const params = ev.context.params
+
+		if (!params)
+			throw createError({ statusCode: 400, statusMessage: 'Missing ID' })
+
+		const deletedProjectRegistry = await prisma.registry.delete({
+			where: { id: params.id },
+		})
+		return { deletedProjectRegistry }
+	} catch (e) {
+		if (e instanceof Error) {
+			throw createError({ statusCode: 400, statusMessage: e.message })
+		}
+	}
+})
