efac81fcca11b8f5d48f7918f0e4288f7951ff00
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-08-03 18:02:16 -0300

    remove external link icon from sidebarl ist

--- src/components/project/sidebar-list.server.vue
+++ src/components/project/sidebar-list.server.vue
@@ -4,10 +4,10 @@
 </script>
 
 <template>
-	<aside class="grid grid-rows-3 mt-6 place-items-center">
+	<aside class="grid h-full grid-rows-3 mt-6 overflow-y-auto place-items-center ">
 		<div
 			v-for="item in projects"
-			class="flex flex-row items-center justify-between w-full p-3 text-sm border-b-2"
+			class="container flex flex-row items-center justify-between w-full p-3 mx-auto text-sm border-b-2"
 		>
 			<div class="flex flex-col justify-center">
 				<h2 class="font-semibold">{{ item.name }}</h2>
@@ -20,9 +20,7 @@
 				>
 			</div>
 			<div class="flex flex-row items-center space-x-3">
-				<button :to="`/home/project/${item.id}`" :title="`View ${item.name}`">
-					<Icon name="uil:external-link-alt" class="w-5 h-5 hover:opacity-80" />
-				</button>
+				
 				<NuxtLink :to="`/home/project/${item.id}`" :title="`View ${item.name}`">
 					<Icon name="uil:external-link-alt" class="w-5 h-5 hover:opacity-80" />
 				</NuxtLink>
