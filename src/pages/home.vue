d27eb8b97610c682c2d1bc47bc4bf7f8f79ea9f8
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-08-03 10:56:38 -0300

    chore: update home menu anchors in order to display project home if user's in its path

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,14 +1,19 @@
 <script setup lang="ts">
 	definePageMeta({ auth: true })
+	const route = useRoute()
+	const isRouteOnProjectPath = computed(() =>
+		route.path.includes('/home/project')
+	)
 </script>
 
 <template>
 	<div>
 		<header class="p-5 border-b-2">
 			<nav
-				class="container flex flex-row items-center justify-between max-w-4xl mx-auto "
+				class="container flex flex-row items-center justify-between max-w-4xl mx-auto"
 			>
-				<NuxtLink to="/home">Home</NuxtLink>
+				<NuxtLink to="/home/project" v-if="isRouteOnProjectPath">Projects</NuxtLink>
+				<NuxtLink to="/home" v-else>Home</NuxtLink>
 				<HomeMenubar />
 			</nav>
 		</header>
