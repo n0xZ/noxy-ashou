ae6782f2035beb711d5ee405c1da39687ec65657
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-08-08 14:33:19 -0300

    fix update registry query result variable name

--- src/server/api/v1/project/[id]/registry/index.get.ts
+++ src/server/api/v1/project/[id]/registry/index.get.ts
@@ -6,8 +6,10 @@ export default defineEventHandler(async (ev) => {
 		const params = ev.context.params
 		if (!params)
 			throw createError({ statusCode: 400, statusMessage: 'Missing ID' })
-		const existingProject = await registriesByProjectId({ projectId: params.id })
-		return { existingProject }
+		const existingRegistries = await registriesByProjectId({
+			projectId: params.id,
+		})
+		return { existingRegistries }
 	} catch (e) {
 		if (e instanceof Error)
 			throw createError({ statusCode: 500, statusMessage: e.message })
