795bf25d946993da578d2d7604f13f6ace0099fe
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-08-14 14:58:55 -0300

    fix: update login title & heading referring to Ashou

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -5,7 +5,7 @@
 			navigateAuthenticatedTo: '/home',
 		},
 	})
-	useSeoMeta({ title: 'Ashou - Sign in' })
+	useSeoMeta({ title: 'Webmetrics - Sign in' })
 	const { signIn } = useAuth()
 	const runtime = useRuntimeConfig()
 	const callbackUrl = runtime.app.baseURL + 'home'
@@ -19,14 +19,14 @@
 			<h2
 				class="text-4xl font-extrabold tracking-tight text-center scroll-m-20 lg:text-5xl"
 			>
-				Sign in to Ashou!
+				Sign in to Webmetrics!
 			</h2>
 			<UiParagraph class="text-center"
 				>You're close to organize your projects with its web metrics!</UiParagraph
 			>
 			<button
 				type="button"
-				title="Ingresar a Ashou desde Github"
+				title="Ingresar a Webmetrics desde Github"
 				class="inline-flex items-center self-center justify-center w-full p-3 space-x-1 text-sm font-medium transition-colors rounded-md shadow bg-primary text-primary-foreground hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
 				@click="
 					signIn('github', {
